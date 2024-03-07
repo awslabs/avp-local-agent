ce2999478dd9a065ce56e3dad013ccb50447ad96
Shirom Makkad <shirmak11@gmail.com>
2024-03-07 16:04:40 -0500

    remove readme
    Signed-off-by: Shirom Makkad <shirmak11@gmail.com>

--- examples/simple_semver_check/src/main.rs
+++ examples/simple_semver_check/src/main.rs
@@ -1,12 +1,13 @@
+//! This file can be used as a simple example or as a demo to ensure that there are no semver violations
 //! Sign in with the AWS CLI so that your credentials are saved either in environment variables or in ~/.aws/credentials.
 //! Then, set up AVP in your account and update `POLICY_STORE_ID`
 
-use std::sync::Arc;
 use aws_sdk_verifiedpermissions::Client;
 use cedar_local_agent::public::file::entity_provider::EntityProvider;
 use cedar_local_agent::public::simple::{Authorizer, AuthorizerConfigBuilder};
 use cedar_policy::{Context, Entities, Request};
 use std::env;
+use std::sync::Arc;
 
 fn construct_request() -> Request {
     Request::new(
@@ -27,11 +28,25 @@ async fn main() {
 
     let aws_config = aws_config::load_from_env().await;
     let client = Client::new(&aws_config);
-    let policy_set_provider = avp_local_agent::public::policy_set_provider::PolicySetProvider::from_client(policy_store_id, client).unwrap();
+    let policy_set_provider =
+        avp_local_agent::public::policy_set_provider::PolicySetProvider::from_client(
+            policy_store_id,
+            client,
+        )
+        .unwrap();
     let entity_provider = EntityProvider::default();
 
-    let authorizer = Authorizer::new(AuthorizerConfigBuilder::default().policy_set_provider(Arc::new(policy_set_provider)).entity_provider(Arc::new(entity_provider)).build().unwrap());
+    let authorizer = Authorizer::new(
+        AuthorizerConfigBuilder::default()
+            .policy_set_provider(Arc::new(policy_set_provider))
+            .entity_provider(Arc::new(entity_provider))
+            .build()
+            .unwrap(),
+    );
 
-    let response = authorizer.is_authorized(&construct_request(), &Entities::empty()).await.unwrap();
+    let response = authorizer
+        .is_authorized(&construct_request(), &Entities::empty())
+        .await
+        .unwrap();
     println!("{response:?}")
 }
